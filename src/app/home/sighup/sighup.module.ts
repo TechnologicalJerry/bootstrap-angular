2a2f0aa2c3fd2c7264cbd482f3ae611827fdd493
technologicaljerry <cserajatverma@gmail.com>
2023-03-01 21:09:52 +0530

    add signup module

---
+++ src/app/home/sighup/sighup.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SighupRoutingModule } from './sighup-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SighupRoutingModule
+  ]
+})
+export class SighupModule { }
