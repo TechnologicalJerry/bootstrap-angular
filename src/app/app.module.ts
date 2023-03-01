d9a05b0dd12663735bf399f1d183451d35beb4b0
technologicaljerry <cserajatverma@gmail.com>
2023-03-01 21:26:49 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SideNavComponent } from './dashboard/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent
+
   ],
   imports: [
     BrowserModule,
