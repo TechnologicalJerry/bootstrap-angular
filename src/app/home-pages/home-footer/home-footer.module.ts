e4f162d0f1acb115053c470ab59e0b899de68e5c
technologicaljerry <cserajatverma@gmail.com>
2023-07-06 23:32:22 +0530

    add footer module

---
+++ src/app/home-pages/home-footer/home-footer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeFooterRoutingModule } from './home-footer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeFooterRoutingModule
+  ]
+})
+export class HomeFooterModule { }
