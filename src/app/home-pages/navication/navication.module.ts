48d68d52cefd7bb42c6c67ffd139b0b6d189c74c
technologicaljerry <cserajatverma@gmail.com>
2023-07-06 23:25:04 +0530

    add navication

---
+++ src/app/home-pages/navication/navication.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavicationRoutingModule } from './navication-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NavicationRoutingModule
+  ]
+})
+export class NavicationModule { }
