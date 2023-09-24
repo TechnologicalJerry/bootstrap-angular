dc6c2e3b0df1b0d838db2a97b791087faf11b71e
technologicaljerry <cserajatverma@gmail.com>
2023-09-24 23:35:24 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,7 @@ const routes: Routes = [
     path: 'home-pages',
     loadChildren: () => import('./home-pages/home-pages.module').then(m => m.HomePagesModule)
   },
-  { path: 'header' }
+  { path: 'header', component: HeaderComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
