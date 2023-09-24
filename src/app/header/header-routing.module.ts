c6a330a27af43ecc3a595a3cba701be3480e409c
technologicaljerry <cserajatverma@gmail.com>
2023-09-24 23:31:15 +0530

    Update header-routing.module.ts

--- src/app/header/header-routing.module.ts
+++ src/app/header/header-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header.component';
 
 const routes: Routes = [
-  {path: ''}
+  { path: '', component: HeaderComponent }
 ];
 
 @NgModule({
