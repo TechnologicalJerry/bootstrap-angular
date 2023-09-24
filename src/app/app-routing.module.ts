d674bb392b4734c804dccf7162ca2594539252a9
technologicaljerry <cserajatverma@gmail.com>
2023-09-24 23:35:48 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
+  { path: 'header', component: HeaderComponent, pathMatch: 'full' },
   {
     path: 'dashboard-pages',
     loadChildren: () => import('./dashboard-pages/dashboard-pages.module').then(m => m.DashboardPagesModule)
@@ -11,7 +12,6 @@ const routes: Routes = [
     path: 'home-pages',
     loadChildren: () => import('./home-pages/home-pages.module').then(m => m.HomePagesModule)
   },
-  { path: 'header', component: HeaderComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
