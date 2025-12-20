ba30a2f11dad994892d98693866439db2b928d59
TechnologicalJerry <cserajatverma@gmail.com>
2025-12-20 23:01:55 +0530

    Optimize header component with OnPush strategy
    Added ChangeDetectionStrategy.OnPush to the header component for improved performance. Also imported RouterLink and RouterLinkActive to support routing in the header.

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Header {
-
-}
+export class Header {}
